b4d03ca6536bef630db2a268e9ba4a6e4d29f389
KevinYouu <zdy295@gmail.com>
2024-07-29 10:47:28 +0800

    fix: merge error

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM nginx:alpine
+FROM nginx:alpine-slim
 COPY ./default.conf /etc/nginx/conf.d/default.conf
 ADD ./out /app
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,17 +9,11 @@ class MyDocument extends Document {
         <Head>
           {!isDevelopment && (
             <>
-              {/* <!-- Google Tag Manager --> */}
               <script
-                dangerouslySetInnerHTML={{
-                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WMNH2QX');`,
-                }}
-              />
-              {/* <!-- End Google Tag Manager --> */}
+                defer
+                src="https://stats.kevnu.com/script.js"
+                data-website-id="23b28b3f-0abd-4946-b5c0-b239a80d111d"
+              ></script>
             </>
           )}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
